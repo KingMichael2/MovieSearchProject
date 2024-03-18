0d530bbaf1f7b7816a5d60ea3e9468cf8c25cfda
Michael <mkcruix2@aol.com>
2024-03-18 19:53:36 +0000

    Hit the backend API

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=5c140bc1&
-const movieListEl = document.querySelector(".movie-list");
+const movieListEl = document.querySelector(".movies");
 
 async function main() {
     const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=5c140bc1&");
     const moviesData = await movies.json();
-    movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("");
+    movieListEl.innerHTML = [moviesData].map((movie) => movieHTML(movie)).join("");
 }
 
 main();
-
 
 function movieHTML(movie) {
     return `<div class="movie">
