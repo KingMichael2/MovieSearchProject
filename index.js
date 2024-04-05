22ec6a143e48aa813ecc9393a8e5b7b1f5f819cb
Michael <mkcruix2@aol.com>
2024-04-05 04:30:39 +0100

    Trying to display the result from API

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-const movieListEl = document.querySelector(".movies");
+let movieResult;
+const form = document.querySelector("form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //stops the page from refreshing after entering input.
+  let searchTerm = form.querySelector("input").value;
 
-async function main(searchTerm) {
-  var searchTerm = document.querySelector(".nav__search").value;
+  getMovies(searchTerm);
+});
 
-    const movies = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=5c140bc1&`);
-    const moviesData = await movies.json();
-    movieListEl.innerHTML = [moviesData].map((movie) => movieHTML(movie)).join("");
+async function renderMovies() {
+  const movieList = document.querySelector(".movies");
+
+  movieList.classList += " movies__loading"
+
+  if (!movieResult) {
+    movieResult = await getMovies();
+  }
+
+  movieList.classList.remove("movies__loading")
+
+  const moviesHTML = movieResult
+  .map((movie) => {
+      return `<div class="movie">
+<figure class="movie__img--wrapper">
+   <img class="movie__img" src="${movie.Poster}" alt="" />
+   </figure> 
+   <div class="movie__title">
+       ${movie.Title}
+   </div>
+   <div class="movie__year">
+     ${movie.Year}
+   </div>
+   <div class="movie__genre">
+     ${movie.Genre}
+   </div>
+ </div>`;
+    })
+    .join("");
+
+  movieList.innerHTML = moviesHTML;
 }
 
-main();
+setTimeout(() => {
+  renderMovies();
+}, 1000);
 
-function movieHTML(movie) {
-    return `<div class="movie">
-    <figure class="movie__img--wrapper">
-       <img class="movie__img" src="${movie.Poster}" alt="" />
-       </figure> 
-       <div class="movie__title">
-           ${movie.Title}
-       </div>
-       <div class="movie__year">
-         ${movie.Year}
-       </div>
-       <div class="movie__genre">
-         ${movie.Genre}
-       </div>
-     </div>`
+async function getMovies(searchTerm) {
+  const request = await fetch(
+    `https://www.omdbapi.com/?apikey=5c140bc1&s=${searchTerm}`);
+  const movies = await request.json();
+  console.log(movies);
 }
